fd6a5c2c13e1cb889301ba75daad97760de2b208
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-15 18:32:38 +0530

    Add files via upload

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CartService } from './cart.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent
+{
+  cart:any[]=[];
+  constructor(private cartService : CartService,private router:Router){}
+
+  ngOnInit():void
+  {
+    this.cartService
+
+  }
+  
+
+
+}
