145fd3cd29b5e2b59408af088e898f28f92e45a1
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:41:48 +0530

    Update PaymentsService.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/PaymentsService.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/PaymentsService.java
@@ -1,79 +1,45 @@
-package com.example.bookstore.bookstoremanagement.service;
-
+// Import necessary classes and annotations.
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.example.bookstore.bookstoremanagement.entity.Payments;
 import com.example.bookstore.bookstoremanagement.repository.PaymentsRepository;
 
+// Define a service class for Payments related operations.
 @Service
-
 public class PaymentsService {
 
-@Autowired
+    @Autowired
+    PaymentsRepository paymentrepository;
 
-PaymentsRepository  paymentrepository;
+    // Retrieve all payments.
+    @Transactional(readOnly = true)
+    public List<Payments> getallPayments() {
+        return paymentrepository.findAll();
+    }
 
- 
+    // Retrieve a payment by its ID.
+    @Transactional(readOnly = true)
+    public Payments getPaymentbyId(int id) {
+        Optional<Payments> pp = paymentrepository.findById(id);
+        if (pp.isPresent())
+            return pp.get();
+        return null;
+    }
 
- 
+    // Insert or modify a payment.
+    @Transactional
+    public boolean insertorModify(Payments payment) {
+        return paymentrepository.save(payment) != null;
+    }
 
-@Transactional(readOnly=true)
-
-public List<Payments> getallPayments(){
-
-    return paymentrepository.findAll();
-
-}
-
- 
-
- 
-
-  @Transactional(readOnly=true)
-
-  public Payments getPaymentbyId(int id) {
-
-    Optional<Payments> pp= paymentrepository.findById(id);
-
-    if(pp.isPresent())
-
-        return pp.get();
-
-    return null;
-
-      
-
-  }
-
-  @Transactional
-
-  
-
-  public boolean insertorModify(Payments  payment) {
-
-    return paymentrepository.save(payment)!=null;
-
-      
-
-  
-
-  }
-
-  @Transactional
-
-  public boolean deletepayment(int id) {
-
-         long count=paymentrepository.count();
-
-         paymentrepository.deleteById(id);
-
-            return count> paymentrepository.count();
-
-  }
-
+    // Delete a payment by its ID.
+    @Transactional
+    public boolean deletepayment(int id) {
+        long count = paymentrepository.count();
+        paymentrepository.deleteById(id);
+        return count > paymentrepository.count();
+    }
 }
