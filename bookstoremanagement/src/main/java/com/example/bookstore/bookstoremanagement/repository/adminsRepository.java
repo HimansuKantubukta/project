ae42f953ffc11f8629e7a512ded405eca94cd17c
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:25:13 +0530

    Update adminsRepository.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/adminsRepository.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/adminsRepository.java
@@ -1,11 +1,15 @@
+// This is the package declaration for the repository interface.
 package com.example.bookstore.bookstoremanagement.repository;
 
+// Import necessary Spring Data JPA repository interface.
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Import the admins entity that this repository is associated with.
 import com.example.bookstore.bookstoremanagement.entity.admins;
 
-
-
-public interface adminsRepository extends JpaRepository<admins, Integer>{
-	admins findAdminIdByEmailAndPassword(String email, String password);
+// Define the adminsRepository interface that extends JpaRepository for admins entities.
+public interface adminsRepository extends JpaRepository<admins, Integer> {
+    
+    // Custom query method to find an admin by email and password.
+    admins findAdminIdByEmailAndPassword(String email, String password);
 }
