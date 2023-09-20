fef8722bd0e249b541376ae6464093ef9100d017
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:41:00 +0530

    Update OrdersService.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/OrdersService.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/OrdersService.java
@@ -1,40 +1,44 @@
-package com.example.bookstore.bookstoremanagement.service;
-
+// Import necessary classes and annotations.
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.example.bookstore.bookstoremanagement.entity.Orders;
 import com.example.bookstore.bookstoremanagement.repository.ordersRepository;
 
+// Define a service class for Orders related operations.
 @Service
 public class OrdersService {
-@Autowired
-ordersRepository  orderrepository;
-@Transactional(readOnly=true)
-public List<Orders> getAlldetails(){
-    return orderrepository.findAll();
+    @Autowired
+    ordersRepository orderrepository;
 
-}
-@Transactional(readOnly=true)
-public Orders getoredresbyId(int id) {
-    Optional<Orders> oo= orderrepository.findById(id);
-    if(oo.isPresent())
-        return oo.get();
-    return null;
+    // Retrieve all orders.
+    @Transactional(readOnly = true)
+    public List<Orders> getAlldetails() {
+        return orderrepository.findAll();
+    }
 
-}
-@Transactional
-public boolean insertOrmodify(Orders order) {
-    return orderrepository.save(order)!=null;
-}
-@Transactional
-  public boolean deleteByid(int id) {
-     long count=orderrepository.count();
-     orderrepository.deleteById(id);
-    return count>orderrepository.count();
-}
+    // Retrieve an order by its ID.
+    @Transactional(readOnly = true)
+    public Orders getoredresbyId(int id) {
+        Optional<Orders> oo = orderrepository.findById(id);
+        if (oo.isPresent())
+            return oo.get();
+        return null;
+    }
+
+    // Insert or modify an order.
+    @Transactional
+    public boolean insertOrmodify(Orders order) {
+        return orderrepository.save(order) != null;
+    }
+
+    // Delete an order by its ID.
+    @Transactional
+    public boolean deleteByid(int id) {
+        long count = orderrepository.count();
+        orderrepository.deleteById(id);
+        return count > orderrepository.count();
+    }
 }
