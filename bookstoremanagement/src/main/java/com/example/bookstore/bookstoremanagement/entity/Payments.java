e906d20db423c921d92cff2e3a5449d3a0b20acc
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:17:19 +0530

    Update Payments.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/entity/Payments.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/entity/Payments.java
@@ -1,144 +1,109 @@
+// This is the package declaration for the entity class.
 package com.example.bookstore.bookstoremanagement.entity;
 
+// Import necessary annotations from the Jakarta Persistence API.
 import jakarta.persistence.Column;
-
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
-
 import jakarta.persistence.GenerationType;
-
 import jakarta.persistence.Id;
-
 import jakarta.persistence.JoinColumn;
-
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
- 
-
+// Define the entity class named "Payments."
 @Entity
 @Table(name="payments")
-
-
 public class Payments
-
 {
+    // Declare the primary key field for Payments, annotated with @Id and generated using Identity strategy.
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="pay_id")
+    private int paymentId;
+    
+    // Declare a field for the customer ID.
+    @Column(name="cust_id")
+    private int customer_id;
+    
+    // Declare a field for the order date.
+    @Column(name="order_date")
+    private String order_date;
+    
+    // Declare a field for the tax amount.
+    @Column(name="tax")
+    private int taxmoney;
+    
+    // Declare a field for the total amount.
+    @Column(name="total")
+    private int totalmoney;
 
- 
+    // Default constructor for Payments.
+    public Payments() {}
 
-        @Id
+    // Parameterized constructor for Payments.
+    public Payments(int paymentId, int customer_id, String order_date, int taxmoney, int totalmoney) {
+        this.paymentId = paymentId;
+        this.customer_id = customer_id;
+        this.order_date = order_date;
+        this.taxmoney = taxmoney;
+        this.totalmoney = totalmoney;
+    }
 
-        @GeneratedValue(strategy=GenerationType.IDENTITY)
+    // Getter method for paymentId.
+    public int getPaymentId() {
+        return paymentId;
+    }
 
-        @Column(name="pay_id")
+    // Setter method for paymentId.
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
 
-        private int paymentId;
+    // Getter method for customer_id.
+    public int getCustomer_id() {
+        return customer_id;
+    }
 
-        @Column(name="cust_id")
+    // Setter method for customer_id.
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
 
-        private int customer_id;
+    // Getter method for order_date.
+    public String getOrder_date() {
+        return order_date;
+    }
 
-        @Column(name="order_date")
+    // Setter method for order_date.
+    public void setOrder_date(String order_date) {
+        this.order_date = order_date;
+    }
 
-        private String order_date;
+    // Getter method for taxmoney.
+    public int getTaxmoney() {
+        return taxmoney;
+    }
 
-        @Column(name="tax")
+    // Setter method for taxmoney.
+    public void setTaxmoney(int taxmoney) {
+        this.taxmoney = taxmoney;
+    }
 
-        private int taxmoney;
+    // Getter method for totalmoney.
+    public int getTotalmoney() {
+        return totalmoney;
+    }
 
-        @Column(name="total")
-
-        private int totalmoney;
-
-        public Payments() {}
-
-        public Payments(int paymentId, int customer_id, String order_date, int taxmoney, int totalmoney) {
-
-            this.paymentId = paymentId;
-
-            this.customer_id = customer_id;
-
-            this.order_date = order_date;
-
-            this.taxmoney = taxmoney;
-
-            this.totalmoney = totalmoney;
-
-        }
-
-        public int getPaymentId() {
-
-            return paymentId;
-
-        }
-
-        public void setPaymentId(int paymentId) {
-
-            this.paymentId = paymentId;
-
-        }
-
-        public int getCustomer_id() {
-
-            return customer_id;
-
-        }
-
-        public void setCustomer_id(int customer_id) {
-
-            this.customer_id = customer_id;
-
-        }
-
-        public String getOrder_date() {
-
-            return order_date;
-
-        }
-
-        public void setOrder_date(String order_date) {
-
-            this.order_date = order_date;
-
-        }
-
-        public int getTaxmoney() {
-
-            return taxmoney;
-
-        }
-
-        public void setTaxmoney(int taxmoney) {
-
-            this.taxmoney = taxmoney;
-
-        }
-
-        public int getTotalmoney() {
-
-            return totalmoney;
-
-        }
-
-        public void setTotalmoney(int totalmoney) {
-
-            this.totalmoney = totalmoney;
-
-        }
-
-        
-
-        
-
-//        @OneToOne
-//
-//        @JoinColumn(name="cus_id")
-//
-//        private Customers customer;
-
-        
-
- 
-
+    // Setter method for totalmoney.
+    public void setTotalmoney(int totalmoney) {
+        this.totalmoney = totalmoney;
+    }
+    
+    // The following section is commented out. You can uncomment it when needed.
+    
+    // Uncomment this section if you want to establish a OneToOne relationship with the Customers entity.
+//    @OneToOne
+//    @JoinColumn(name="cus_id")
+//    private Customers customer;
 }
