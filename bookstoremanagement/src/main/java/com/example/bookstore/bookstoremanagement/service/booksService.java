09486617c2b8e50140b144d9da3272ff40d30701
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:42:39 +0530

    Update booksService.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/booksService.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/booksService.java
@@ -1,93 +1,37 @@
-package com.example.bookstore.bookstoremanagement.service;
-
+// Import necessary classes and annotations.
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.example.bookstore.bookstoremanagement.entity.Books;
 import com.example.bookstore.bookstoremanagement.repository.Booksrepository;
 
+// Define a service class for Books related operations.
 @Service
 public class booksService {
 
-@Autowired
+    @Autowired
+    Booksrepository booksRepository;
 
-Booksrepository  booksRepository;
+    // Retrieve all books.
+    @Transactional(readOnly = true)
+    public List<Books> getAllBooks() {
+        return booksRepository.findAll();
+    }
 
-    
+    // Retrieve a book by its ID.
+    @Transactional(readOnly = true)
+    public Books getBookById(int Book_id) {
+        Optional<Books> ct = booksRepository.findById(Book_id);
+        if (ct.isPresent())
+            return ct.get();
+        return null;
+    }
 
-    
-
-     @Transactional(readOnly=true)
-
-     public List<Books> getAllBooks(){
-
-          return booksRepository.findAll();
-
-
-     }
-
-     @Transactional(readOnly=true)
-
-     public Books getBookById(int Book_id) {
-
-    Optional<Books> ct= booksRepository.findById(Book_id);
-
-    if(ct.isPresent())
-
-        return ct.get();
-
-    return null;
-
-     }
-
-//     
-//
-//      @Transactional
-//
-//      public boolean insertIntoBooks(Books book) {
-//
-//        return booksRepository.save(book)!=null;
-//
-//          
-//
-//      }
-//
-//      
-//
-//    @Transactional
-//
-//    public boolean deletByBookId(int book_id) {
-//
-//         long count=booksRepository.count();
-//
-//         booksRepository.deleteById(book_id);
-//
-//            return count> booksRepository.count();
-//
-//    }
-//    
-       
-//     @Transactional
-//
-// 	public List<Books>findingByTitle(String title)
-// 	{
-// 		 return booksRepository.findByTitle(title);
-// 	}
-
-	@Transactional
-
-	public List<Books> searchBooksBy(String name) {
-
-		return booksRepository.searchBook(name);
-
-	
-	}
-	
-
-
+    // Search books by name using a provided search query.
+    @Transactional
+    public List<Books> searchBooksBy(String name) {
+        return booksRepository.searchBook(name);
+    }
 }
-
