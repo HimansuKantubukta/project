4e97b4762eee2ee642cce4f209640ee936aed070
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:38:00 +0530

    Update AuthorService.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/AuthorService.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/AuthorService.java
@@ -1,74 +1,45 @@
-package com.example.bookstore.bookstoremanagement.service;
-
+// Import necessary classes and annotations.
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.example.bookstore.bookstoremanagement.entity.Author;
 import com.example.bookstore.bookstoremanagement.repository.AuthorRepository;
 
+// Define a service class for Author related operations.
 @Service
 public class AuthorService 
 {
-	@Autowired
-	AuthorRepository authorRepository;
-	
-	  @Transactional(readOnly=true)
+    @Autowired
+    AuthorRepository authorRepository;
+    
+    // Retrieve all authors.
+    @Transactional(readOnly=true)
+    public List<Author> getAllAuthors(){
+        return authorRepository.findAll();
+    }
 
-	     public List<Author> getAllAuthors(){
+    // Retrieve an author by their ID.
+    @Transactional(readOnly=true)
+    public Author getAuthorsById(int Author_id) {
+        Optional<Author> at = authorRepository.findById(Author_id);
+        if (at.isPresent())
+            return at.get();
+        return null;
+    }
 
-	          return authorRepository.findAll();
+    // Insert a new author.
+    @Transactional
+    public boolean insertIntoAuthors(Author author) {
+        return authorRepository.save(author) != null;
+    }
 
-	     
-
-	     }
-
-	     @Transactional(readOnly=true)
-
-	     public Author getAuthorsById(int Author_id) {
-
-	    Optional<Author> at= authorRepository.findById(Author_id);
-
-	    if(at.isPresent())
-
-	        return at.get();
-
-	    return null;
-
-	     }
-
-	     
-
-	      @Transactional
-
-	      public boolean insertIntoAuthors(Author author) {
-
-	        return authorRepository.save(author)!=null;
-
-	          
-
-	      }
-
-	      
-
-	    @Transactional
-
-	    public boolean deletByauthorId(int Author_id) {
-
-	         long count=authorRepository.count();
-
-	         authorRepository.deleteById(Author_id);
-
-	            return count> authorRepository.count();
-
-	    }
-
-	}
-	
-	
-	
-
-
+    // Delete an author by their ID.
+    @Transactional
+    public boolean deletByauthorId(int Author_id) {
+        long count = authorRepository.count();
+        authorRepository.deleteById(Author_id);
+        return count > authorRepository.count();
+    }
+}
