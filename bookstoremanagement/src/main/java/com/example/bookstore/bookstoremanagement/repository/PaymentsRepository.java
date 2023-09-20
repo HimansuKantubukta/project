a10e43310671857610ffe4f410061f3fd56f641e
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:23:54 +0530

    Update PaymentsRepository.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/PaymentsRepository.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/PaymentsRepository.java
@@ -1,14 +1,19 @@
+// This is the package declaration for the repository interface.
 package com.example.bookstore.bookstoremanagement.repository;
 
+// Import necessary Spring Data JPA repository interface.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 
+// Import the Payments entity that this repository is associated with.
 import com.example.bookstore.bookstoremanagement.entity.Payments;
 
-public interface PaymentsRepository extends JpaRepository<Payments,Integer> {
-	  @Procedure(procedureName = "makepayment")
-	  void makePayment(int custId);
-
-	}
+// Define the PaymentsRepository interface that extends JpaRepository for Payments entities.
+public interface PaymentsRepository extends JpaRepository<Payments, Integer> {
+    
+    // Custom stored procedure call to make a payment.
+    @Procedure(procedureName = "makepayment")
+    void makePayment(int custId);
+}
 
 
