15ec373c3e6db4fb591ff83579dac90a4186045d
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:23:07 +0530

    Update CustomersRepository.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/CustomersRepository.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/CustomersRepository.java
@@ -1,39 +1,33 @@
+// This is the package declaration for the repository interface.
 package com.example.bookstore.bookstoremanagement.repository;
 
+// Import necessary Spring Data JPA repository interface.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
+// Import necessary Java time library.
 import java.time.LocalDate;
 
+// Import the Customers entity that this repository is associated with.
 import com.example.bookstore.bookstoremanagement.entity.Customers;
 
-public interface CustomersRepository extends JpaRepository<Customers,Integer>
-{
-	@Procedure("customer_signup")
-	void insertinto(
-			@Param("cust_name") String name,
-			@Param("cust_mobile") long mobile,
-			@Param("email_id") String emailId,
-			@Param("postal_code") int postal_code,
-			@Param("state") String state,
-			@Param("password") String password,
-			@Param("createdat") LocalDate date
-			);
+// Define the CustomersRepository interface that extends JpaRepository for Customers entities.
+public interface CustomersRepository extends JpaRepository<Customers, Integer> {
+    
+    // Custom stored procedure call to insert customer data.
+    @Procedure("customer_signup")
+    void insertinto(
+            @Param("cust_name") String name,
+            @Param("cust_mobile") long mobile,
+            @Param("email_id") String emailId,
+            @Param("postal_code") int postal_code,
+            @Param("state") String state,
+            @Param("password") String password,
+            @Param("createdat") LocalDate date
+    );
 
-//	@Query("SELECT c FROM Customers c WHERE c.email_id = :email_id AND c.password = :password")
-//
-//    Customers validateLogin(
-//
-//        @Param("email_id") String email,
-//
-//        @Param("password") String password
-//
-//    );
-	
-//	@Query(nativeQuery = true,value = "select * from customers where email_id =:email and password =:password")
-//	  Customers findByEmail_idAndPassword(String email, String password);
-
-	Customers findCustomersByEmailIdAndPassword(String emailId, String password);
+    // Custom query method to find customers by emailId and password.
+    Customers findCustomersByEmailIdAndPassword(String emailId, String password);
 }
