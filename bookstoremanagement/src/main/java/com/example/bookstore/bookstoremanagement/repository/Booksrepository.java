f85afc3a8d1d2f7e55da991f97bc1f3c42373869
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:21:03 +0530

    Update Booksrepository.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/Booksrepository.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/Booksrepository.java
@@ -1,30 +1,21 @@
+// This is the package declaration for the repository interface.
 package com.example.bookstore.bookstoremanagement.repository;
 
-
-import java.util.List;
+// Import necessary Spring Data JPA repository interface.
 import org.springframework.data.jpa.repository.JpaRepository;
+// Import necessary annotations and classes for custom query.
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+// Import the Books entity that this repository is associated with.
 import com.example.bookstore.bookstoremanagement.entity.Books;
 
-public interface Booksrepository extends JpaRepository<Books,Integer>
-{
-	
-//	List<Books> findByTitle(String title);
-
-	@Query("SELECT b FROM Books b WHERE b.title LIKE :name OR b.category LIKE  :name")
+// Define the Booksrepository interface that extends JpaRepository for Books entities.
+public interface Booksrepository extends JpaRepository<Books, Integer> {
+    
+    // Custom query to search for books by title or category using a SQL-like query.
+    @Query("SELECT b FROM Books b WHERE b.title LIKE :name OR b.category LIKE  :name")
     List<Books> searchBook(
         @Param("name") String name
-    );	
-	
-
-	
+    );
 }
-
-
-
-
-
-
-
