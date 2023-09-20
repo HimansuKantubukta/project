e3b8271d39e2ff700ee43d3be117b63f92102f39
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:20:05 +0530

    Update AuthorRepository.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/AuthorRepository.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/repository/AuthorRepository.java
@@ -1,17 +1,13 @@
+// This is the package declaration for the repository interface.
 package com.example.bookstore.bookstoremanagement.repository;
 
+// Import necessary Spring Data JPA repository interface.
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Import the Author entity that this repository is associated with.
 import com.example.bookstore.bookstoremanagement.entity.Author;
 
-
-
-
-
-
-
-
-public interface AuthorRepository extends JpaRepository<Author,Integer>
-{
-
+// Define the AuthorRepository interface that extends JpaRepository for Author entities.
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    // No additional code is added here since this interface simply extends JpaRepository.
 }
