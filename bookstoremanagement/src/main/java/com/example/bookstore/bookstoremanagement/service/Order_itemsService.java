fdaca722ce96c8516b33718d8db6f0cdb303dee4
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:40:13 +0530

    Update Order_itemsService.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/Order_itemsService.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/service/Order_itemsService.java
@@ -1,67 +1,44 @@
-package com.example.bookstore.bookstoremanagement.service;
-
+// Import necessary classes and annotations.
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.example.bookstore.bookstoremanagement.entity.Order_items;
 import com.example.bookstore.bookstoremanagement.repository.Order_itemsRepository;
 
+// Define a service class for Order_items related operations.
 @Service
-
 public class Order_itemsService {
+    @Autowired
+    Order_itemsRepository order_itemsrepository;
 
-@Autowired
+    // Retrieve all order items.
+    @Transactional
+    public List<Order_items> getallitems() {
+        return order_itemsrepository.findAll();
+    }
 
-Order_itemsRepository order_itemsrepository;
+    // Retrieve an order item by its ID.
+    @Transactional(readOnly = true)
+    public Order_items getByid(int order_id) {
+        Optional<Order_items> ot = order_itemsrepository.findById(order_id);
+        if (ot.isPresent())
+            return ot.get();
+        return null;
+    }
 
-@Transactional
+    // Insert or modify an order item.
+    @Transactional
+    public boolean insertorModify(Order_items order) {
+        return order_itemsrepository.save(order) != null;
+    }
 
-public List<Order_items> getallitems(){
-
-    return order_itemsrepository.findAll();
-
-}
-
-
-@Transactional(readOnly=true)
-
-  public Order_items getByid(int order_id) {
-
-    Optional<Order_items> ot=order_itemsrepository.findById(order_id);
-
-    if(ot.isPresent())
-
-        return ot.get();
-
-    return null;
-
-}
-
-@Transactional
-
-public boolean insertorModify(Order_items order) {
-
-    return order_itemsrepository.save(order)!=null;
-
-
-}
-
-@Transactional
-
-public boolean  deleteByid(int id) {
-
-     long count=order_itemsrepository.count();
-
-     order_itemsrepository.deleteById(id);
-
-    return count> order_itemsrepository.count();
-
-}
-
- 
-
+    // Delete an order item by its ID.
+    @Transactional
+    public boolean deleteByid(int id) {
+        long count = order_itemsrepository.count();
+        order_itemsrepository.deleteById(id);
+        return count > order_itemsrepository.count();
+    }
 }
