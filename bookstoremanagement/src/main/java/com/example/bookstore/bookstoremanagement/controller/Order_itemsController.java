ca432dadf07562a44faaa57311332b6f0ddbc81a
HimansuKantubukta <138876187+HimansuKantubukta@users.noreply.github.com>
2023-09-20 10:35:00 +0530

    Update Order_itemsController.java

--- bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/controller/Order_itemsController.java
+++ bookstoremanagement/src/main/java/com/example/bookstore/bookstoremanagement/controller/Order_itemsController.java
@@ -1,9 +1,5 @@
-package com.example.bookstore.bookstoremanagement.controller;
-
+// Import necessary classes and annotations.
 import java.util.List;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,52 +12,48 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.bookstore.bookstoremanagement.entity.Order_items;
 import com.example.bookstore.bookstoremanagement.service.Order_itemsService;
 
-@CrossOrigin(origins= {"http://localhost:4200"})
+// Enable Cross-Origin Resource Sharing (CORS) for this controller.
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RequestMapping("/Order_items")
 @RestController
-public class Order_itemsController
-{
-	@Autowired
-	Order_itemsService itemService;
+public class Order_itemsController {
 
-	@GetMapping(value="/",produces="application/json")
-	 public ResponseEntity<List<Order_items>>getallitems()
-	 {
-		  List<Order_items> t1=itemService.getallitems();
-		  if(t1.size()!=0)
-			return new ResponseEntity<List<Order_items>>(t1,HttpStatus.OK);
-			  return new ResponseEntity<List<Order_items>>(t1,HttpStatus.NOT_FOUND);
+    @Autowired
+    Order_itemsService itemService;
 
-	 }
+    // Get all order items.
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity<List<Order_items>> getallitems() {
+        List<Order_items> t1 = itemService.getallitems();
+        if (t1.size() != 0)
+            return new ResponseEntity<List<Order_items>>(t1, HttpStatus.OK);
+        return new ResponseEntity<List<Order_items>>(t1, HttpStatus.NOT_FOUND);
+    }
 
+    // Insert a new order item.
+    @PostMapping(value = "/", consumes = "application/json")
+    public HttpStatus insertOrder_items(@RequestBody Order_items order) {
+        if (itemService.insertorModify(order))
+            return HttpStatus.OK;
+        return HttpStatus.NOT_MODIFIED;
+    }
 
- 
+    // Modify an existing order item.
+    @PutMapping(value = "/", consumes = "application/json")
+    public HttpStatus ModifyOrder_items(@RequestBody Order_items order) {
+        if (itemService.insertorModify(order))
+            return HttpStatus.OK;
+        return HttpStatus.NOT_MODIFIED;
+    }
 
-	 @PostMapping(value="/",consumes="applications/json")
-	 public HttpStatus insertOrder_items(@RequestBody Order_items order) {
-		 if(itemService.insertorModify(order))
-			 return HttpStatus.OK;
-		 return HttpStatus.NOT_MODIFIED;
-	}
-
-
-	 @PutMapping(value="/",consumes="applications/json")
-	 public HttpStatus ModifyOrder_items(@RequestBody Order_items order) {
-		 if(itemService.insertorModify(order))
-			 return HttpStatus.OK;
-		 return HttpStatus.NOT_MODIFIED;
-	}
-
-
-	 @DeleteMapping("/{customerId}")
-	 public HttpStatus deleteOrder_items(@PathVariable int Id)
-	 {
-		 if(itemService.deleteByid(Id))
-			 return HttpStatus.OK;
-		 return HttpStatus.NOT_FOUND;
-	 }
+    // Delete an order item by its ID.
+    @DeleteMapping("/{customerId}")
+    public HttpStatus deleteOrder_items(@PathVariable int Id) {
+        if (itemService.deleteByid(Id))
+            return HttpStatus.OK;
+        return HttpStatus.NOT_FOUND;
+    }
 }
